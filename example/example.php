5c4f5fd5e52aae8140b6cd7f3cba99871db318a5
Vojta Svoboda <vojtasvoboda.cz@gmail.com>
2014-09-10 17:34:33 +0200

    Prepare styles

--- example/example.php
+++ example/example.php
@@ -5,6 +5,9 @@ require '../src/Hasher/Hasher.php';
 // gets hash (max 128 chars)
 $password = isset($_GET['p']) ? $_GET['p'] : false;
 
+// init
+$hashes = array();
+
 // if we have password
 if ($password) {
 
@@ -13,6 +16,9 @@ if ($password) {
 
     // get cleaned password for HTML input
     $password = $hasher->getPassword();
+
+    $hashes[] = array('title' => 'first', 'hash' => 'asdf');
+    $hashes[] = array('title' => 'second', 'hash' => 'fads');
 
 }
 
@@ -23,32 +29,15 @@ if ($password) {
         <meta charset="utf-8">
         <title>Password Hashed</title>
         <style>
-            body, input {
-                font-family: Consolas, 'Liberation Mono', Courier, monospace;
-                font-size: 18px;
-            }
-            #wrapper {
-                margin: 0 auto;
-                width: 920px;
-                border: 1px solid #aaa;
-                padding: 0 20px 30px;
-            }
-            #p {
-                font-size: 140%;
-                padding: 5px 10px;
-                width: 400px;
-            }
-            #submit {
-                display: block;
-                padding: 15px;
-                width: 100px;
-            }
-            .clr {
-                float: none;
-                width: 100%;
-                height: 1px;
-                clear: both;
-            }
+            * { padding: 0; margin: 0; }
+            body, input { font-family: Consolas, 'Liberation Mono', Courier, monospace; font-size: 18px; }
+            #wrapper { margin: 0 auto; width: 920px; border: 1px solid #aaa; padding: 0 20px 30px; }
+            #p { font-size: 140%; padding: 5px 10px; width: 400px; }
+            #submit { display: block; width: 150px; height: 40px; }
+            .hashes { margin-top: 15px; border: 1px solid #aaa; }
+            tr { text-align: left; }
+            th, td { padding: 8px 8px 4px; }
+            .clr { float: none; width: 100%; height: 1px; clear: both; }
         </style>
     </head>
     <body>
@@ -57,15 +46,17 @@ if ($password) {
             <form action="" method="get">
                 <label for="p">Please enter a password:</label><br />
                 <input type="text" name="p" id="p" placeholder="Password max 128 characters." value="<?=$password;?>" />
-                <input type="submit" value="Hash!" id="submit" />
+                <input type="submit" value="Shake it baby!" id="submit" />
             </form>
             <div class="clr"></div>
             <?php if(!empty($hashes)) {
+            echo '<table class="hashes" rules="all">';
                 foreach($hashes as $hash) {
-                    echo '<div class="hash">';
-                    echo '<span class="title">' . $hash['title'] . '</span><span>' . $hash['hash'] . '</span>';
-                    echo '</div>';
+                    echo '<tr class="hash">';
+                    echo '<th class="title">' . $hash['title'] . '</th><td>' . $hash['hash'] . '</td>';
+                    echo '</tr>';
                 }
+            echo '</table>';
             } ?>
         </div>
     </body>
