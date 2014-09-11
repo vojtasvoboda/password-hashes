d080084d374b6f6c1b17d1219bf4de06f1d597d3
Vojta Svoboda <vojtasvoboda.cz@gmail.com>
2014-09-11 22:39:53 +0200

    GitHub badge, typo

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Password hasher
 
-Showcase of hashing passwords.
+Showcase of hashing passwords. Available at [http://hasher.7ka.cz/](http://hasher.7ka.cz/)
+
+## Changelog
+
+Version 1.0.0 - 2014-09-11 - see tag v1.0.0 - only plain HTML, CSS, JavaScript and PHP
--- example/example.php
+++ example/example.php
@@ -50,11 +50,13 @@ if (isset($_GET['p'])) {
             table th { text-align: left; font-weight: bold; width: 100px; padding: 4px 4px 0; border: 1px solid #aaa; }
             table td { padding: 2px 4px; border: 1px solid #aaa; }
             table input { width: 100%; outline: none; padding: 4px 0 0 0; }
+            #badge { display: none; }
             @media (min-width: 860px) {
                 body { margin-top: 15px; margin-bottom: 15px; }
                 #wrapper { margin: 0 auto; width: 800px; border: 1px solid #999; padding: 20px; }
                 h1 { font-size: 280%; }
                 #password { padding: 6px 8px 4px; width: 782px; }
+                #badge { display: block; position: absolute; top: 0; right: 0; }
             }
         </style>
     </head>
@@ -76,6 +78,11 @@ if (isset($_GET['p'])) {
                 }
             echo '</table>';
             } ?>
+        </div>
+        <div id="badge">
+            <a href="https://github.com/vojtasvoboda/password-hashes">
+                <img src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png">
+            </a>
         </div>
     </body>
 </html>
