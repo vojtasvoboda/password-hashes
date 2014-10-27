5017b7f090c3c3e89ca58d26c3f5f6f7b522c5f6
Vojta Svoboda <vojtasvoboda.cz@gmail.com>
2014-10-28 00:09:56 +0100

    Google Tag Manager

--- example/example.php
+++ example/example.php
@@ -61,6 +61,17 @@ if (isset($_GET['p'])) {
         </style>
     </head>
     <body>
+
+        <!-- Google Tag Manager -->
+        <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-NPTN6J"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-NPTN6J');</script>
+        <!-- End Google Tag Manager -->
+
         <div id="wrapper">
             <h1>Password Hasher 1.0</h1>
             <form action="" method="get">
@@ -79,10 +90,12 @@ if (isset($_GET['p'])) {
             echo '</table>';
             } ?>
         </div>
+
         <div id="badge">
             <a href="https://github.com/vojtasvoboda/password-hashes">
                 <img src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png">
             </a>
         </div>
+
     </body>
 </html>
