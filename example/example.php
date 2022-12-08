c680956f9333b8dc3ccc25a4bb7fb0003b8df866
Vojta Svoboda <vojtasvoboda.cz@gmail.com>
2022-12-08 23:19:43 +0100

    Update GTM container ID

--- example/example.php
+++ example/example.php
@@ -63,13 +63,13 @@ if (isset($_GET['p'])) {
     <body>
 
         <!-- Google Tag Manager -->
-        <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-NPTN6J"
+        <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-KJKGRTD"
         height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
         <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-NPTN6J');</script>
+        })(window,document,'script','dataLayer','GTM-KJKGRTD');</script>
         <!-- End Google Tag Manager -->
 
         <div id="wrapper">
